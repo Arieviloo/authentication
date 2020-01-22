d3d7cbf80930d4215ba7f6928b68c58660530887
Jadiê <jadieoliveira00@gmail.com>
2020-01-22 19:17:27 -0300

    first

---
+++ js/authentication.js
@@ -0,0 +1,7 @@
+var btnLogin = document.getElementById("btnLogin");
+var inputEmail = document.querySelector("#inputEmail");
+var inputPassword = document.querySelector("#inputPassword");
+
+btnLogin.addEventListener("click", () => {
+  console.log("deu bom");
+});
