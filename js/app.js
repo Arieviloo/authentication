8de374ea783adbab6239acaa29561b2b94106cb2
Jadiê Oliveira <jadieoliveira00@gmail.com>
2020-01-28 19:05:14 -0300

    signIn

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyCddD429Drdbej7ePYIR-WFR6uEUIbvqkA",
-  authDomain: "authentication-a5055.firebaseapp.com",
-  databaseURL: "https://authentication-a5055.firebaseio.com",
-  projectId: "authentication-a5055",
-  storageBucket: "authentication-a5055.appspot.com",
-  messagingSenderId: "689274407378",
-  appId: "1:689274407378:web:c4dc00a9e76daebf14a00b"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+(() => {
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCddD429Drdbej7ePYIR-WFR6uEUIbvqkA",
+    authDomain: "authentication-a5055.firebaseapp.com",
+    databaseURL: "https://authentication-a5055.firebaseio.com",
+    projectId: "authentication-a5055",
+    storageBucket: "authentication-a5055.appspot.com",
+    messagingSenderId: "689274407378",
+    appId: "1:689274407378:web:c4dc00a9e76daebf14a00b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+})();
--- js/authentication.js
+++ js/authentication.js
@@ -3,5 +3,21 @@ var inputEmail = document.querySelector("#inputEmail");
 var inputPassword = document.querySelector("#inputPassword");
 
 btnLogin.addEventListener("click", () => {
-  console.log("deu bom");
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(
+      document.querySelector("#inputEmail").value,
+      document.querySelector("#inputPassword").value
+    )
+    .then(() => {
+      console.log("deu bom");
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage);
+
+      // ...
+    });
 });
